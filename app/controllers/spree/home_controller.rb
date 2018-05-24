506c9aa2c1abd9aeef0daa0bccd4fa3e70c9b58b
OSCAR <ochng6654@gmail.com>
2018-05-24 19:40:01 +0800

    removing cache

---
+++ app/controllers/spree/home_controller.rb
@@ -0,0 +1,22 @@
+module Spree
+  class HomeController < Spree::StoreController
+    helper 'spree/products'
+    respond_to :html
+
+    def index
+      if spree_current_user
+        Rails.cache.clear
+        @searcher = build_searcher(params.merge(include_images: true))
+        @products = @searcher.retrieve_products
+        @products = @products.includes(:possible_promotions) if @products.respond_to?(:includes)
+        @taxonomies = Spree::Taxonomy.includes(root: :children)
+      else
+        Rails.cache.clear
+        @searcher = build_searcher(params.merge(include_images: true))
+        @products = @searcher.retrieve_products
+        @products = @products.includes(:possible_promotions) if @products.respond_to?(:includes)
+        @taxonomies = Spree::Taxonomy.includes(root: :children)
+      end
+    end
+  end
+end

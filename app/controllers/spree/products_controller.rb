44df106d6aa389752d90c5c669cdb3cccc6494cb
OSCAR <ochng6654@gmail.com>
2018-06-18 15:00:21 +0800

    always show featured product

--- app/controllers/spree/products_controller.rb
+++ app/controllers/spree/products_controller.rb
@@ -11,6 +11,7 @@ module Spree
       @searcher = build_searcher(params.merge(include_images: true))
       @products = @searcher.retrieve_products
       @products = @products.includes(:possible_promotions) if @products.respond_to?(:includes)
+      @products = @products.where(featured: true)
       @taxonomies = Spree::Taxonomy.includes(root: :children)
     end
 

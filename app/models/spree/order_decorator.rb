fe0e444f3b3ff20aa1a72f4ea0473351e5f828b7
OSCAR <ochng6654@gmail.com>
2018-06-29 16:04:59 +0800

    changes in receiving reward

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -20,6 +20,40 @@ Spree::Order.class_eval do
     true
   end
 
+  include Spree::TransactionRegistrable
+  attr_accessor :referral_code, :affiliate_code, :can_activate_associated_partner
+
+  has_many :transactions, as: :commissionable, class_name: 'Spree::CommissionTransaction', dependent: :restrict_with_error
+  belongs_to :affiliate, class_name: 'Spree::Affiliate'
+
+  state_machine.after_transition to: :complete, do: :create_commission_transaction
+
+  private
+  def create_commission_transaction
+    # register_commission_transaction(affiliate) if affiliate.present?
+    referred = Spree::Referral.where('lower(code) = ?', user.referred_record.referral.code.downcase).first
+    store_credit = create_store_credits(referred.user) if referrer_eligible?(referred.user)
+  end
+
+  def create_store_credits(referrer)
+    referrer.store_credits.create(amount: referral_amount(referrer),
+                                  category_id: referral_store_credit_category.try(:id),
+                                  created_by: Spree::User.admin.try(:first),
+                                  currency: Spree::Config.currency)
+  end
+
+  def referral_store_credit_category
+    @store_credit_category ||= Spree::StoreCreditCategory.find_or_create_by(name: Spree::StoreCredit::REFERRAL_STORE_CREDIT_CATEGORY)
+  end
+
+  def referral_amount(referrer)
+    referrer.referral_credits || Spree::Config[:referral_credits]
+  end
+
+  def referrer_eligible?(user)
+    Spree::Config[:referrer_benefit_enabled] && user.referrer_benefit_enabled
+  end
+
 end
 
 # Validate on state change

28535c2e8dfd18615c5aa02ebdd5ed59271ee21f
Oscar Chng <ochng6654@gmail.com>
2018-07-07 16:05:58 +0800

    Update home_controller_decorator.rb

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -6,13 +6,13 @@ module Spree
         @taxon = Spree::Taxon.find_by!(slug: params[:id])
         @products = Spree::Product.in_sale.in_taxon(@taxon)
       else
-        #@products = Spree::Product.in_sale
+        @products = Spree::Product.in_sale
         
-        @searcher = build_searcher(params.merge(include_images: true))
-        @products = @searcher.retrieve_products
-        @products = @products.includes(:possible_promotions) if @products.respond_to?(:includes)
-        @products = @products.joins(master: :sale_prices)
-                          .where(spree_sale_prices: {enabled: true})
+        #@searcher = build_searcher(params.merge(include_images: true))
+        #@products = @searcher.retrieve_products
+        #@products = @products.includes(:possible_promotions) if @products.respond_to?(:includes)
+        #@products = @products.joins(master: :sale_prices)
+        #                  .where(spree_sale_prices: {enabled: true})
       end
       @taxonomies = Spree::Taxonomy.includes(root: :children)
     end

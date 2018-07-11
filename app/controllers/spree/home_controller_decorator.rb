9ccfab443808aef89b230bb14a3171c6a0144114
OSCAR <ochng6654@gmail.com>
2018-07-11 12:46:54 +0800

    remove discontinued from sale

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -7,8 +7,11 @@ module Spree
         @products = Spree::Product.in_sale.in_taxon(@taxon)
       else
         @products = Spree::Product.in_sale
+        binding.pry
+        @products.where(discontinued: nil)
+        @products.where('discontinue_on is null OR discontinue_on > ?', Time.now)
         @products = @products.page(params[:page]).per(12)
-        
+
         #@searcher = build_searcher(params.merge(include_images: true))
         #@products = @searcher.retrieve_products
         #@products = @products.includes(:possible_promotions) if @products.respond_to?(:includes)

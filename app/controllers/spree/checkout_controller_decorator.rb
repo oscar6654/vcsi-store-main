17e69efdde0499d72e32123523e81d3587e96b88
OSCAR <ochng6654@gmail.com>
2018-07-13 15:27:13 +0800

    remove 500 minmum

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,10 +1,10 @@
 Spree::CheckoutController.class_eval do
-  def ensure_checkout_allowed
-    check = @order.checkout_allowed?
-    unless check == true
-      redirect_to spree.cart_path, :flash => {:error => "Order needs to be greater than ₱500 to checkout" }
-    end
-  end
+  # def ensure_checkout_allowed
+  #   check = @order.checkout_allowed?
+  #   unless check == true
+  #     redirect_to spree.cart_path, :flash => {:error => "Order needs to be greater than ₱500 to checkout" }
+  #   end
+  # end
 
 
   require 'slack-notifier'

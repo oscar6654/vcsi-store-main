e345bca200b6e718a8db8354435dedb00db5cfff
OSCAR <ochng6654@gmail.com>
2018-07-31 12:39:49 +0800

    put in sale as featured

--- app/controllers/spree/home_controller.rb
+++ app/controllers/spree/home_controller.rb
@@ -11,6 +11,7 @@ module Spree
         @products = @products.includes(:possible_promotions) if @products.respond_to?(:includes)
         # binding.pry
         # @products = @products.where(featured: true)
+        @products = @products.in_sale
         @taxonomies = Spree::Taxonomy.includes(root: :children)
       else
         # Rails.cache.clear
@@ -18,8 +19,22 @@ module Spree
         @products = @searcher.retrieve_products
         @products = @products.includes(:possible_promotions) if @products.respond_to?(:includes)
         # @products = @products.where(featured: true)
+        @products = @products.in_sale
         @taxonomies = Spree::Taxonomy.includes(root: :children)
       end
     end
+
+    def build_searcher(params)
+      searcher_class.new(params).tap do |searcher|
+        searcher.current_user = try_spree_current_user
+        searcher.current_currency = current_currency
+      end
+    end
+
+    def searcher_class
+      @searcher_class = Spree::Core::Search::Base
+    end
+
+
   end
 end
